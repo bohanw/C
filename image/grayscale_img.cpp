64a41c8ee99cf12db55734c7795e8086e232a9c6
bohanw <bohanwang93@gmail.com>
2024-09-15 00:51:36 -0700

    2d conv with vector container

--- image/grayscale_img.cpp
+++ image/grayscale_img.cpp
@@ -8,11 +8,14 @@ private:
     int width;
     int height;
     uint8_t* buffer;
+    std::vector<std::vector<uint8_t> > imageIn2d;
 
 public:
     // Constructor
     GrayscaleImage(int w, int h) : width(w), height(h) {
         buffer = new uint8_t[width * height];
+        imageIn2d = std::vector<std::vector<uint8_t> >(height, std::vector<uint8_t>(width, 0)); 
+
     }
     
     // Destructor
@@ -69,7 +72,6 @@ public:
         delete[] newBuffer;
     }
 
-    
 
     // Method to print the image
     void print() const {
@@ -82,6 +84,48 @@ public:
     }
 };
 
+//Following is representation of image 2D vector. May not actually represent the most efficient
+// image represnetation but for practicie purpose
+static int getPixelWithPaddingFromInput(int x, int y, int w, int h, const std::vector<std::vector<int> >& input) {
+    if (x < 0 || x >= w || y < 0 || y >= h) {
+        return 0;  // Return 0 for padding areas
+    }
+    return input[y][x];
+}
+
+static void convolutionWithStride(const std::vector<std::vector<int> >& input,
+                            const std::vector<std::vector<int> >& kernel,
+                            std::vector<std::vector<int> >& output,
+                            int stride)
+{
+    int output_width = 0;
+    int in_row = input.size();
+    int in_col = input[0].size();
+    int padding = kernel[0].size() / 2;
+    int k = kernel.size();
+
+    int offset = kernel.size() / 2;
+    // output dimension with stride calculation : https://www.appsilon.com/post/convolutional-neural-networks
+    int out_row = (in_row - k + 2 * padding) / stride + 1;
+    int out_col = (in_col - k + 2 * padding) / stride + 1;
+    output.resize(out_row, std::vector<int>(out_col, 0));
+
+    for(int y = 0; y < out_row;y++){
+        for(int x = 0; x < out_col;x++){
+            int sum = 0;
+
+            for(int ky = -1 * offset;ky <= offset;ky++){
+                for(int kx = -1 * offset;kx <= offset;kx++) {
+                    int in_y = y * stride + ky;
+                    int in_x = x * stride + kx;
+                    sum += getPixelWithPaddingFromInput(in_y, in_x, in_row, in_col, input) * kernel[ky + offset][kx + offset];
+                }
+            }
+            output[y][x] = sum;
+        }
+    } 
+    
+}
 int main() {
     // Create a sample grayscale image (5x5)
     GrayscaleImage img(5, 5);
@@ -118,5 +162,31 @@ int main() {
     std::cout << "\nConvolution with Zero-Padding:" << std::endl;
     img.print();
 
+    std::vector<std::vector<int> > input2d = 
+    {
+        {10, 20, 30, 40, 50},
+        {20, 30, 40, 50, 60},
+        {30, 40, 50, 60, 70},
+        {40, 50, 60, 70, 80},
+        {50, 60, 70, 80, 90}
+    };
+    std::vector<std::vector<int>> knl = {
+        { -1,  0,  1 },
+        { -2,  0,  2 },
+        { -1,  0,  1 }
+    }; 
+    std::vector<std::vector<int> > output;
+    convolutionWithStride(input2d, knl, output, 2);
+
+    std::cout << "Convolution Result with Stride of 2:" << std::endl;
+    for (const auto& row : output) {
+        for (int val : row) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
+
+
